7c0d54d6d1d48717ed7f83af1366ecf80836169e
dj3129-bit <dleowns3129@gmail.com>
2025-10-20 21:10:29 +0900

    Add files via upload

---
+++ sql/comment.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `comment` (
+	`commentid` INT NOT NULL AUTO_INCREMENT,
+	`recipeid` INT NOT NULL,
+	`commentcontent` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`commentdate` DATE NULL DEFAULT NULL,
+	`isreport` TINYINT(1) NULL DEFAULT NULL,
+	`userid` VARCHAR(32) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	PRIMARY KEY (`commentid`, `recipeid`) USING BTREE,
+	INDEX `recipeid` (`recipeid`) USING BTREE,
+	INDEX `userid` (`userid`) USING BTREE,
+	CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`recipeid`) REFERENCES `recipepost` (`recipeid`) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `recipeuser` (`userid`) ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+COLLATE='utf8mb4_0900_ai_ci'
+ENGINE=InnoDB
+;
